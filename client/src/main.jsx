0cbe27487fbb289a788de6a378fdf3d9f648d6c2
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-07-31 19:38:13 -0600

    added a main.css

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import App from './app'
+import './main.css'
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>

59455c82da9d7c2f7f5739824c077e349102d111
yaneyalvarado <alvyaney@gmail.com>
2023-08-01 20:44:36 -0600

    updated url and function to successfully fetch the API and retrieve data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -21,22 +21,33 @@
 //     });
 // };
 
-// make a search to amadeius flights api
-// export const searchFlights = (query) => {
-//     return fetch('test.api.amadeus.com/cCugx4gjvjABhqA6SOhdxyI0z1BoVZWV');
-// };
 
-function amadeiusFetch () {
-fetch ("test.api.amadeus.com/v2/shopping/flight-offers", {
 
-     headers:{ 
-        "Authorization": "Bearer SIWGHvruAFR6YatnjpjyXRCiGQhn"
-},
-    method: "GET"
-}).then((res) => {
-    return res.json();
-}).then((data) => {
-    console.log (data);
-})};
+function amadeiusFetch(){
+    const token = "hblCUGeTjn0KQQL8VygxEo1UvDZP"
+    // let url = "test.api.amadeus.com/v2/shopping/flight-offers";
+    fetch("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2023-11-01&adults=1&max=2", {
+        method:"GET",
+        headers:{
+            "Content-Type":"application/vnd.amadeus+json",
+            "Authorization":`Bearer ${token}`
+        },
+        mode:"cors",
+        catch:"default"
+    }).then(function(response){
+        console.log(response);
+        if(response.ok){
+            return response.json();
+        }
+        // else{
+        //     throw new Error(error);
+        // }
+    }).then(function(data){
+        console.log(data);
+        //query data here
+    }).catch(function(error){
+        console.log(error);
+    });
+}
 
 amadeiusFetch();

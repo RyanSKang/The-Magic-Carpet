dcdfa75a13d56fe04f0c2c225ed1460b9429017f
Jason-Kodama <124485518+Jason-Kodama@users.noreply.github.com>
2023-08-06 17:49:03 -0600

    Revert "updated search.jsx to render data from API. Also updated API.js to ex…"

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,7 @@
-
-export function amadeiusFetch(searchInput){
-    const token = "tiLV3ThgxJAFUISwAnwjrwXOe37E"
-
+function amadeusFetch(){
+    const token = "hblCUGeTjn0KQQL8VygxEo1UvDZP"
     // let url = "test.api.amadeus.com/v2/shopping/flight-offers";
-   return fetch(`https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=${searchInput}&departureDate=2023-11-01&adults=1&max=2`, {
+    fetch("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2023-11-01&adults=1&max=2", {
         method:"GET",
         headers:{
             "Content-Type":"application/vnd.amadeus+json",
@@ -11,24 +9,20 @@ export function amadeiusFetch(searchInput){
         },
         mode:"cors",
         catch:"default"
-    })
-    // .then(function(response){
-    //     // console.log(response);
-    //     if(response.ok){
-    //         return response.json();
-    //     }
-    //     // else{
-    //     //     throw new Error(error);
-    //     // }
-    // }).then(function(data){
-    //     console.log(data.data);
-    //     console.log(searchInput);
-    //     return data.data;
-    //     //query data here
-    // }).catch(function(error){
-    //     console.log(error);
-    // });
+    }).then(function(response){
+        console.log(response);
+        if(response.ok){
+            return response.json();
+        }
+        // else{
+        //     throw new Error(error);
+        // }
+    }).then(function(data){
+        console.log(data);
+        //query data here
+    }).catch(function(error){
+        console.log(error);
+    });
 }
 
-// amadeiusFetch();
-
+amadeusFetch();

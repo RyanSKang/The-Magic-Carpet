dc53d7b6a14a2f0255cf735ee01fb09120ec9549
Jason <jasonkodama@gmail.com>
2023-08-06 21:48:12 -0600

    many changes

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,21 +1,73 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 // Importing CSS
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 // Importing Components
-import Home from './Components/Home/Home'
+import Home from './pages/home'
 import NavbarMain from './Components/Navbar/Navbar'
 import Search from './Components/Search/search'
+import Signup from './pages/signup';
+import Login from './pages/login';
+import ResultsPage from './pages/resultsPages'
+
+const httpLink = createHttpLink({
+    uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('id_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : '',
+        },
+    };
+});
+
+const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+});
 
 const App= () => {
     return(
-        <div>
-            <NavbarMain/>
-            <Home/>
-            <Search />
-        </div>
+        <ApolloProvider client={client}>
+            <Router>
+                <div>
+                    <NavbarMain/>
+                    <Routes>
+                        <Route 
+                            path="/"
+                            element={<Home />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={<Signup />}
+                        />
+                        <Route
+                            path="/login"
+                            element={<Login />}
+                        />
+                        <Route
+                            path="/results"
+                            element={<ResultsPage />}
+                        />
+                    </Routes>
+                    <Search />
+                </div>
+            </Router>
+
+        </ApolloProvider>
     )
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './App.css';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
-import ResultsPage from './pages/resultsPages';
+// import ResultsPage from './pages/resultsPages';
+// import Signup from './pages/signup';
+// import Login from './pages/login';
 
 // Importing a Browser Router to link multiple pages
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 
 // Creating Browser Router to link multiple pages into application
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "results",
-    element: <ResultsPage></ResultsPage>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//   },
+//   {
+//     path: "results",
+//     element: <ResultsPage></ResultsPage>
+//   },
+//   {
+//     path: "signup",
+//     element: <Signup />
+//   },
+//   {
+//     path: "login",
+//     element: <Login />
+//   }
+// ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <RouterProvider router={router} />
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

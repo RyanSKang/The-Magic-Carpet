d1b5677479cd84bcf95897cedc6863a1267d265e
yaneyalvarado <alvyaney@gmail.com>
2023-08-06 18:37:23 -0600

    fixed syntax errors with search.jsx

--- client/src/Components/Search/search.jsx
+++ client/src/Components/Search/search.jsx
@@ -1,83 +1,96 @@
 import React, { useState } from "react";
 
 // Importing BootStrap CDN
-import Button from 'react-bootstrap/Button';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 // Importing react date range
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
 
+// Importing amedius API
+import { amadeiusFetch } from "../../utils/API";
 
-const Search = ({ setResults }) => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+const Search = () => {
+  const [searchedFlights, setSearchedFlights] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-  // API Token
-  const token = "hblCUGeTjn0KQQL8VygxEo1UvDZP"
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-  // API Fetch
-  const amadeusFetch = () => {
-    fetch("https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2023-11-01&adults=1&max=2", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/vnd.amadeus+json",
-        "Authorization": `Bearer ${token}`
-      },
-      mode: "cors",
-      catch: "default"
-    }).then(function (response) {
-      console.log(response);
-      if (response.ok) {
-        return response.json();
+    if (!searchInput) {
+      return false;
+    }
+
+    try {
+      const response = await amadeiusFetch(searchInput);
+      if (!response.ok) {
+        // throw new Error("something went wrong!");
       }
-      // else{
-      //     throw new Error(error);
-      // }
-    }).then(function (data) {
-      console.log(data);
-      //query data here
-      setResults(data);
-    }).catch(function (error) {
-      console.log(error);
-    });
+
+      const { data } = await response.json();
+      const flightData = data.map((flight) => ({
+        price: flight.price.base,
+        id: flight.id,
+      }));
+      console.log(flightData);
+      setSearchedFlights(flightData);
+      setSearchInput("");
+    } catch (err) {
+      // console.error(err);
+    }
   };
 
-  const handleChange = (value) => {
-    // setInput(value);
-    amadeusFetch(value);
-  }
-
+  // const handleChange = (value) => {
+  //   // setInput(value);
+  //   amadeusFetch(value);
+  // };
   return (
     <>
-      <ButtonGroup aria-label="Basic example" as={Col} className="position-relative rounded-pill top-100 start-50 translate-middle bi bi-caret-down-fill">
+      <ButtonGroup
+        aria-label="Basic example"
+        as={Col}
+        className="position-relative rounded-pill top-100 start-50 translate-middle bi bi-caret-down-fill"
+      >
         <Button variant="secondary">Economy</Button>
         <Button variant="secondary">Business</Button>
         <Button variant="secondary">First Class</Button>
       </ButtonGroup>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
         <Row>
           <Form.Group as={Col} controlId="formGridState" className="searchForm">
             <Form.Label>Destination From</Form.Label>
-            <Form.Control type="text" placeholder="Enter an Origin" ></Form.Control>
+            <Form.Control
+              type="text"
+              placeholder="Enter a Destination"
+              name="searchInput"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            ></Form.Control>
           </Form.Group>
-          <Form.Group as={Col} controlId="formGridState">
+          <Row>
+          <Form.Group as={Col} controlId="formGridState" className="searchForm">
             <Form.Label>Destination To</Form.Label>
             <Form.Control type="text" placeholder="Enter a Destination" ></Form.Control>
           </Form.Group>
         </Row>
+        <Form>
         <Row>
-          <Form.Group className="top-25 searchForm" as={Col} controlId="formGridState">
-            <Form.Label>Check In | Check Out</Form.Label>
+          <Form.Group
+            className="top-25 searchForm"
+            as={Col}
+            controlId="formGridState"
+          >
+            {/* <Form.Label>Check In | Check Out</Form.Label>
             <DatePicker
               selected={startDate}
               selectsStart
               startDate={startDate}
               endDate={endDate}
-              onChange={date => setStartDate(date)}
+              onChange={(date) => setStartDate(date)}
             />
             <DatePicker
               selected={endDate}
@@ -85,10 +98,12 @@ const Search = ({ setResults }) => {
               startDate={startDate}
               endDate={endDate}
               minDate={startDate}
-              onChange={date => setEndDate(date)}
-            />
+              onChange={(date) => setEndDate(date)}
+            /> */}
           </Form.Group>
-          <Form.Group className='searchForm' as={Col} controlId="formGridState">
+          </Row>
+          </Form>
+          <Form.Group className="searchForm" as={Col} controlId="formGridState">
             <Form.Label>How many Travelers?</Form.Label>
             <Form.Select defaultValue="Choose...">
               <option>Choose...</option>
@@ -103,12 +118,16 @@ const Search = ({ setResults }) => {
             </Form.Select>
           </Form.Group>
         </Row>
-        <Button onClick={(e) => handleChange(e.target.value)} variant="primary" type="search">
+        {/* <Button variant="primary" type="submit">
+
+      </Row> */}
+        <Button onClick={(e) => Search(e.target.value)} variant="primary" type="search">
+
           Search
         </Button>
       </Form>
     </>
   )
-}
+};
 
-export default Search
+export default Search;

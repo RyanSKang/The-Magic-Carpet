58cff07a3104f29ab1a2c38e0e6cd29d6afc3cde
Ryan Kang <ryan.sanhae.kang@gmail.com>
2023-08-08 17:29:12 -0600

    results page css

--- client/src/Components/Results/results.jsx
+++ client/src/Components/Results/results.jsx
@@ -1,18 +1,21 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import Card from "react-bootstrap/Card";
-// import Button from 'react-bootstrap/Button';
-// import Offcanvas from 'react-bootstrap/Offcanvas';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import { useLocation } from "react-router-dom";
 
 const ResultsPage = ({ ...props }) => {
-  // const [show, setShow] = useState(false);
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const location = useLocation();
   console.log(location.state);
   return (
     <>
-      <div>Results Page</div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}>Results Page</div>
       {location.state.flightData.map((flight, i) => (
         <Card>
           <p>{flight.originLocation}</p>
@@ -22,6 +25,18 @@ const ResultsPage = ({ ...props }) => {
           <p>{flight.iataCode}</p>
           <p>{flight.id}</p>
           <p>{flight.duration}</p> 
+          <Button variant="primary" onClick={handleShow} className="me-2">
+                Purchase Flight
+            </Button>
+            <Offcanvas show={show} onHide={handleClose} {...props}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Payment Checkout</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
         </Card>
       ))}
       {/* <div>
